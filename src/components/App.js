beeca1e7f3bef7ace1a98d8e7b443e9239bec1fd
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-08-12 15:35:40 +0100

    Add Search Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../styles/App.css';
+import Search from'./Search.js/Search'
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Hello World!
-      </p>
+        <img
+          className="nasa-logo" 
+          src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+          alt="nasaLogo"
+        />
+        <Search />
     </div>
   );
 }
+
+// ReactDOM.render(
+//   <App />, document.getElementById('root')
+// );
 
 export default App;
