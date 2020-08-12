055246c90d877091c9126165b23027c0b5355e78
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-08-12 13:17:56 +0100

    Add tne foundation of the tech-app

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Hello World!
+      </p>
+    </div>
+  );
+}
+
+export default App;
