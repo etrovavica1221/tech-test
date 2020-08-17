4bb4f9ff2f363767e2dd1e3bb569cb1bc6c19ef5
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-08-17 23:54:01 +0100

    Updates

---
+++ precache-manifest.4ec674afb2447080b3d81cf3b8c4db12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4bbc47a870b65908d34252621165e1",
+    "url": "/tech-test/index.html"
+  },
+  {
+    "revision": "317d6ca847f1b5d0278a",
+    "url": "/tech-test/static/css/main.5a7f5a37.chunk.css"
+  },
+  {
+    "revision": "d45131b9c917751f18db",
+    "url": "/tech-test/static/js/2.92fb1879.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tech-test/static/js/2.92fb1879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317d6ca847f1b5d0278a",
+    "url": "/tech-test/static/js/main.3c950e1b.chunk.js"
+  },
+  {
+    "revision": "e53cdf7258c9f40463c1",
+    "url": "/tech-test/static/js/runtime-main.a73da4ab.js"
+  }
+]);
